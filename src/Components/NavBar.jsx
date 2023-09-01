83d1b82a59c552292f7174e664776363d18266a3
Joseph Ogba <josephogba247@gmail.com>
2023-09-01 09:12:07 +0000

    Initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div> <nav className=" relative container bg-red-500">
+    Save changes
+  </nav>
+  </div>
+  )
+}
+
+export default NavBar
